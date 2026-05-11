36aad942e583ddf926e0caeebdfc2959e5591c8b
Dysthesis <antheoraviel@protonmail.com>
2026-05-11 18:33:53 +1000

    feat(ui): use solaire-mode to visually distinguish main buffers from popups et. al.

--- src/init.el
+++ src/init.el
@@ -124,7 +124,16 @@
 ;; Theme
 (add-to-list 'custom-theme-load-path
              (expand-file-name "themes" user-emacs-directory))
-(load-theme 'noir t)
+(load-theme 'noir t) ;; My own custom theme
+(use-package solaire-mode
+  :ensure t
+  :after (lackluster-theme)
+  :config
+  (setq solaire-mode-supported-themes
+        '(noir))
+  (when-let ((theme (car custom-enabled-themes)))
+    (solaire-mode--prepare-for-theme-a theme))
+  (solaire-global-mode +1))
 
 (use-package ligature
   :ensure t
