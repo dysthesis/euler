a3e325a665fe9845988116c3bf15c97cb2522357
Dysthesis <antheoraviel@protonmail.com>
2026-05-11 12:06:23 +1000

    feat(packaging): include nerd-icons and treesitter grammars

--- nix/packages/emacsWithPackages.nix
+++ nix/packages/emacsWithPackages.nix
@@ -1,9 +1,12 @@
 {
   emacsWithPackagesFromUsePackage,
-  emacs-unstable-pgtk,
-  cfg,
+  emacs-unstable-pgtk, cfg,
 }:
 emacsWithPackagesFromUsePackage {
   package = emacs-unstable-pgtk;
   config = "${cfg}/init.el";
+  extraEmacsPackages = epkgs: with epkgs; [
+    nerd-icons
+    treesit-grammars.with-all-grammars
+  ];
 }
