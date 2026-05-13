e88546e78c52d634e843292f8fdfd9ca0afd23be
Dysthesis <antheoraviel@protonmail.com>
2026-05-13 16:10:40 +1000

    fix(font): still too big

--- src/init.el
+++ src/init.el
@@ -142,7 +142,7 @@
   (let* ((font-size
 	  (let* ((hostname (car (split-string (system-name) "\\." t)))
 		 (size-by-hostname '(("deimos" . 9)
-				     ("phobos" . 11)))
+				     ("phobos" . 9)))
 		 (default-size 9))
 	    (or (cdr (assoc hostname size-by-hostname))
 		default-size)))
